bcf1a9bd06e571d396fe89823f45ca745efc963e
jpandradesouza <jp.andradesousa@hotmail.com>
2021-06-01 05:06:32 -0300

    final

--- bd.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE Login;
-USE Login;
-
-CREATE TABLE Usuarios (CPF INT PRIMARY KEY,
-					   email VARCHAR(60),
-                       endereco VARCHAR(75),
-                       senha VARCHAR(20));
